592b62915ab0fc031b6b74734ba468af6dc74b42
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2024-12-10 00:19:58 +0000

    main.js: Rewrote addBookToLibrary function and removed call to it in Book constructor

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ function Book(title, author, pages, read) {
     this.author = author;
     this.pages = pages;
     this.read = read;
-    addBookToLibrary(this);
 };
 
 Book.prototype.isRead = function() {
@@ -30,8 +29,13 @@ Book.prototype.isRead = function() {
     }
 };
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
+function addBookToLibrary() {
+    let title = document.querySelector("#title").value;
+    let author = document.getElementById("author").value;
+    let pages = document.getElementById("pages").value;
+    let read = document.getElementById("read").checked;
+    let newBook = new Book(title, author, pages, read);
+    console.log(newBook)
 }
 
 function removeBook(index) {
