0db931a7ccc170d2bbf2ecd6c56deb9f39911017
Bamidele Adewuyi <57547443+BamideleAdewuyi@users.noreply.github.com>
2025-01-09 18:29:47 +0000

    main.js: Added Book class with same arguments as object function

--- main.js
+++ main.js
@@ -20,6 +20,15 @@ function Book(title, author, pages, read) {
     this.read = read;
 };
 
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+}
+
 Book.prototype.toggleRead = function() {
     this.read = !this.read;
 }
